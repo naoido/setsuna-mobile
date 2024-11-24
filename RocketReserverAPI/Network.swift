97937229fe772f15d47e7cc29fba9d1f2da1484e
AnnkoATAMA <annkopan0903@gmail.com>
2024-11-24 15:08:47 +0900

    end

--- RocketReserverAPI/Network.swift
+++ RocketReserverAPI/Network.swift
@@ -10,7 +10,7 @@ class Network {
         let cache = InMemoryNormalizedCache()
         let store = ApolloStore(cache: cache)
         let provider = NetworkInterceptorProvider(client: client, store: store)
-        let url = URL(string: "https://80e8-92-203-95-41.ngrok-free.app/graphql")!
+        let url = URL(string: "https://041c-92-203-95-41.ngrok-free.app/graphql/")!
         let transport = RequestChainNetworkTransport(interceptorProvider: provider, endpointURL: url)
 
         return ApolloClient(networkTransport: transport, store: store)
